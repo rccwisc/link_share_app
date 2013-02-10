ad51e459a52e7c879c7282eded4c4b66a48272a7
MATC Student <rccwisc@gmail.com>
2013-02-10 09:35:30 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LinkShareApp::Application.config.secret_token = '3fd540eee1da43d8344347023c2e9ddd2d99ec9d77b88b278fd5aa370b799807bea62097d7c56e668a072c7e888aefad5fa69ab390a6238686e0879a4bb36028'
