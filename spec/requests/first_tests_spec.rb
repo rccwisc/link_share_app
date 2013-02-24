d49361479e18d79b5d7db3bc7f6984ea847fa880
MATC Student <rccwisc@gmail.com>
2013-02-24 10:34:50 -0600

    last changes for project1

--- spec/requests/first_tests_spec.rb
+++ spec/requests/first_tests_spec.rb
@@ -16,4 +16,19 @@ describe "FirstTests" do
           page.should have_content('New link')
     end
   end
+  
+  describe "Validations"  do
+      
+    it "should have valid http address" do
+        Link.new(:name => "some", :url => 'hppt://foo.com', :saved => "02/02/2012").should_not be_valid
+    end
+    
+    it "should not have nil/blank name" do
+        Link.new(:name => "", :url => 'http://www.foo.com', :saved => "02/02/2012").should_not be_valid
+    end
+    
+    it "should be valid" do
+        Link.new(:name => "some name", :url => 'http://www.foo.com', :saved => "02/02/2012").should be_valid
+    end
+  end
 end
