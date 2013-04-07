b7814320b7e8f9fae8c05b8a10854a7e5a161522
MATC Student <rccwisc@gmail.com>
2013-04-07 18:13:57 -0500

    comment for commit

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 gem 'rails', '3.2.11'
 gem 'bootstrap-sass', '2.1'
+gem 'bcrypt-ruby', '3.0.1'
 
 
 group :development, :test do
@@ -20,8 +21,13 @@ end
 
 gem 'jquery-rails','2.0.2'
 
+group :development do
+  gem 'annotate', '2.5.0'
+end
+
 group :test do
   gem 'capybara', '1.1.2'
+  gem 'factory_girl_rails', '4.1.0'
 end
 
 group :production do
--- app/models/link.rb
+++ app/models/link.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: links
+#
+#  id         :integer          not null, primary key
+#  url        :string(255)
+#  name       :string(255)
+#  saved      :date
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Link < ActiveRecord::Base
   attr_accessible :name, :saved, :url
   validates :url, :format => { :with => /^http/,
